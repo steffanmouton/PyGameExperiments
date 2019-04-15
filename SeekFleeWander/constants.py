c5c67d2b34c91d5510ceb22e6bd6ad449bcb69a9
Steffan Mouton <smouton8993@gmail.com>
2019-04-15 17:54:40 -0500

    Added SeekFleeWander
    Anything other than (1,-1) as initial velocity creates weird behavior.

---
+++ SeekFleeWander/constants.py
@@ -0,0 +1,11 @@
+'''all constants for game'''
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+DEBUG = False
+GOTEEMWHITE = False
+GOTEEMBLACK = False
