1114ff6537ce20b532f156a66bb1ce15ad714554
Steffan Mouton <smouton8993@gmail.com>
2019-04-29 17:59:15 -0500

    Worked on Astar

--- aStar/graph.py
+++ aStar/graph.py
@@ -30,11 +30,11 @@ def gen_nodes(size):
 class Node:
     '''Contains data.'''
     def __init__(self, position = [0,0], value = 0):
-        self._data = value
-        self._pos = position
+        self.data = value
+        self.pos = position
         self.fScore = 0
         self.gScore = 0
-        self.hSCore = 0
+        self.hScore = 0
 
 class Edge:
     '''Connection between two nodes. Takes in parameters of two points to be connected.'''
@@ -55,6 +55,7 @@ class Graph:
         self._edges = edges_param
         if not self.edges:
             self.set_edges(gen_edges(self))
+        self.currentNode = _nodes[0]
 
     def adjacent_edges(self, n):
         '''Takes in a node, returns a list of edges connected to itself'''
@@ -65,6 +66,21 @@ class Graph:
                 adj_edge_list.append(e)
         
         return adj_edge_list
+
+    def adjacent_nodes(self, this_node):
+        connected_edges = adjacent_edges(n)
+        nbrs = []
+
+        for e in connected_edges:
+            for n in e.nodes:
+                if n == this_node:
+                    continue
+                if n in nbrs:
+                    continue
+                nbrs.append(n)
+                
+        return nbrs
+
 
     def render(self, game):
         for n in self.nodes:
@@ -85,3 +101,5 @@ class Graph:
     
     def set_edges(self, e):
         self._edges = e
+
+    
--- aStar/stefmath.py
+++ aStar/stefmath.py
@@ -5,7 +5,7 @@ def find_distance(a, b):
     if a is tuple and b is tuple:
         alpha = (float(a[0]), float(a[1]))
         beta = (float(b[0]), float(b[1]))
-        return float(math.sqrt(pow(beta[0]-alpha[0], 2) + pow(beta[1]-alpha[1], 2)))
+        return float(abs(math.sqrt(pow(beta[0]-alpha[0], 2) + pow(beta[1]-alpha[1], 2))))
 
 def find_vector_from(a, b):
     return (b[0] - a[0], b[1] - a[1])
@@ -21,3 +21,34 @@ def move_agent(ag, dt):
     ag.acceleration = [ag.force[0]/ag.mass, ag.force[1]/ag.mass]
     ag.curVelocity = [ag.curVelocity[0] + (ag.acceleration[0] * dt), ag.curVelocity[1] + (ag.acceleration[1] * dt)]
     ag.pos = [ag.pos[1] + (ag.curVelocity[0] * dt), ag.pos[1] + (ag.curVelocity[1] * dt)]
+
+def manhattan(a, b):
+    return a + b
+
+def a_star(graph, goal, heuristic = manhattan):
+    closedNodes = []
+    discoveredNodes = [graph.currentNode]
+
+    while discoveredNodes.length() > 0:
+        discoveredNodes.sort(fScore) # TODO FIX THIS
+        current = discoveredNodes[0]
+        if current == goal:
+            return
+                
+        discoveredNodes.remove(current)
+        closedNodes.append(current)
+
+        for n in graph.adjacent_nodes(current):
+            if n in closedNodes():
+                continue
+
+            tent_gScore = current.gScore + find_distance(current, n)
+
+            if n not in discoveredNodes():
+                discoveredNodes.append()
+            elif tent_gScore >= n.gScore:
+                continue
+            
+            n.parent = current
+            n.gScore = tent_gScore
+            n.fScore = n.gScore + heuristic(n, goal)
